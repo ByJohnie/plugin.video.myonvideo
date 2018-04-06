13aa9f2d73e4a9539c8f2aba5a5c03fc3f0c9d17
ByJohnie <34856210+ByJohnie@users.noreply.github.com>
2018-04-06 12:15:00 +0300

    Add files via upload

--- default.py
+++ default.py
@@ -18,12 +18,14 @@ catalogue = base64.b64decode('aHR0cDovL215b252aWRlby5jb20vY2F0YWxvZ3VlLzE=')
 movi = base64.b64decode('aHR0cDovL215b252aWRlby5jb20vY2F0ZWdvcnkvMzEvRmlsbWkvMQ==')
 ser = base64.b64decode('aHR0cDovL215b252aWRlby5jb20vY2F0ZWdvcnkvMTkvU2VyaWFsaS8xLw==')
 sernext = base64.b64decode('aHR0cDovL215b252aWRlby5jb20vY2F0ZWdvcnkvMTkvU2VyaWFsaS8xLw==')
+search = base64.b64decode('aHR0cDovL215b252aWRlby5jb20vc2VhcmNoLnBocA==')
 MUA = 'Mozilla/5.0 (Linux; Android 5.0.2; bg-bg; SAMSUNG GT-I9195 Build/JDQ39) AppleWebKit/535.19 (KHTML, like Gecko) Version/1.0 Chrome/18.0.1025.308 Mobile Safari/535.19' #За симулиране на заявка от мобилно устройство
 UA = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:40.0) Gecko/20100101 Firefox/40.0' #За симулиране на заявка от  компютърен браузър
 
 
 #Меню с директории в приставката
 def CATEGORIES():
+        addDir('Търсене',search,11,'DefaultFolder.png')
         addDir('Последно добавени',catalogue,1,movico)
         addDir('Филми',movi,2,movico)
         addDir('Сериали',ser,4,serico)
@@ -48,7 +50,7 @@ def INDEXCATALOGUE(url):
             movie = 'http://myonvideo.com/play:' + idmov
             addLink(title,movie,9,descr,thumbnail)
             br = br + 1
-        if br == 25: #тогава имаме следваща страница и конструираме нейния адрес
+        if br >= 24: #тогава имаме следваща страница и конструираме нейния адрес
             getpage=re.compile('(.*)(\d+)').findall(url)
             for baseurl,page in getpage:
                 newpage = int(page)+1
@@ -115,7 +117,7 @@ def INDEXSERIALS(url):
             getpage=re.compile('(.*)(\d+)').findall(url)
             for baseurl,page in getpage:
                 newpage = int(page)+1
-                newurl = serurl + 'Page/' + str(newpage)
+                newurl = sernext + 'Page/' + str(newpage)
                 #print 'URL OF THE NEXT PAGE IS' + url
                 thumbnail='DefaultFolder.png'
                 addDir('следваща страница>>',newurl,5,thumbnail)
@@ -212,7 +214,36 @@ def PLAY(url):
         try: _addon.resolve_url(stream_url)
         except: t=''
 
-
+def SEARCH(url):
+       keyb = xbmc.Keyboard('', 'Търсачка')
+       keyb.doModal()
+       searchText = ''
+       if (keyb.isConfirmed()):
+           searchText = urllib.quote_plus(keyb.getText())
+           searchText=searchText.replace(' ','+')
+           params = {'t':searchText, 'x':'26', 'y':'18'}
+           req = urllib2.Request(url, urllib.urlencode(params))
+           req.add_header('User-Agent', UA)
+           response = urllib2.urlopen(req)
+           #print 'request page url:' + url
+           data=response.read()
+           response.close()
+           br = 0 #Брояч на видеата в страницата - 24 за този сайт
+           match = re.compile('img src="http.+?-(\d+).jpg" alt="(.+?)"').findall(data)
+           for idmov,title in match:
+            movie = 'http://myonvideo.com/play:' + idmov
+            thumbnail = 'http://myonvideo.com/image-' + idmov + '.jpg'
+            descr = 'Липсва описание'
+            addLink(title,movie,9,descr,thumbnail)
+            br = br + 1
+           if br == 40: #тогава имаме следваща страница и конструираме нейния адрес
+            getpage=re.compile('</div><div class="pagination">\n<b>(.+?)</b><a href="(.+?)."').findall(data)
+            for page,baseurl in getpage:
+                newpage = int(page)+1
+                url = baseurl + str(newpage)
+                #print 'URL OF THE NEXT PAGE IS' + url
+                thumbnail='DefaultFolder.png'
+                addDir('следваща страница>>',url,6,thumbnail)
 
 
 
@@ -333,4 +364,8 @@ elif mode==9:
 elif mode==10:
         print ""+url
         PLAY(url)
+
+elif mode==11:
+        print ""+url
+        SEARCH(url)
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
